5eee43ef839a099b1fa594e7e63ef7f4a7dde561
mbk <zoucongyu1993@hotmail.com>
2024-09-21 16:41:47 +0200

    login/logout

--- CommunityBlogs/django_project/settings.py
+++ CommunityBlogs/django_project/settings.py
@@ -117,8 +117,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = "login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
