93b4f4489ab7b8eda565887345044e07e06e1304
mbk <zoucongyu1993@hotmail.com>
2024-10-13 19:23:33 +0200

    debug

--- CommunityBlogs/blog/views.py
+++ CommunityBlogs/blog/views.py
@@ -22,7 +22,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
 
 
 class UserPostListView(ListView):
@@ -30,7 +30,7 @@ class UserPostListView(ListView):
     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 3
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
