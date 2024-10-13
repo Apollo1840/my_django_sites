32de647c598cb9998354a0ba5893b74e00b0a45b
mbk <zoucongyu1993@hotmail.com>
2024-10-13 17:57:01 +0200

    debug

--- CommunityBlogs/django_project/wsgi.py
+++ CommunityBlogs/django_project/wsgi.py
@@ -18,3 +18,11 @@ application = get_wsgi_application()
 
 sys.path.append('/home/congyu/congyu_program/react/my_django_sites/CommunityBlogs')
 sys.path.append('/home/congyu/congyu_program/react/my_django_sites/CommunityBlogs/django_project')
+
+import logging
+
+logging.basicConfig(stream=sys.stderr)
+logging.getLogger().setLevel(logging.DEBUG)
+
+logging.debug('Python executable: %s', sys.executable)
+logging.debug('sys.path: %s', sys.path)
